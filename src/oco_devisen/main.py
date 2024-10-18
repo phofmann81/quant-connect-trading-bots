9a83838618eced32d226d098edb4fd6b5d233df8
Philipp Hofmann <philipp.hofmann@zalando.de>
2024-10-18 08:51:31 +0200

    fixes

--- src/oco_devisen/main.py
+++ src/oco_devisen/main.py
@@ -14,23 +14,31 @@ class OcODevisenStrategy(QCAlgorithm):
         self._eur_usd = self.add_forex(ticker="EURUSD", resolution=Resolution.MINUTE)
         self._eur_usd_symbol = self._eur_usd.symbol
         self.orders = {}
-        self.pip_size_breakout_padding = 0.00002
-        self.stop_loss_pip_size = 5  # could be related to atr TODO parameterize
         self.bar_window = RollingWindow[QuoteBar](
             61
         )  # TODO learn how to use consolidator for this
         self._atr = self.atr(self._eur_usd_symbol, Resolution.Minute)
 
-        self.pip_size = (
-            self._eur_usd.symbol_properties.minimum_price_variation
-        )  # 1e-05 for eur-usd
-        self.lot_size = int(1 / self.pip_size) + 1
+        self.pip_size = self._eur_usd.symbol_properties.minimum_price_variation * 10
+        self.pip_size_breakout_padding = self.pip_size * 2
+        self.lot_size = (
+            int(1 / self._eur_usd.symbol_properties.minimum_price_variation) + 1
+        )
         self.total_trades = 0
         self.winning_trades = 0
+        self.traded_today = False
 
         self.schedule.on(
             self.date_rules.every_day(), self.time_rules.at(22, 59), self.liquidate
         )
+        self.schedule.on(
+            self.date_rules.every_day(),
+            self.time_rules.at(23, 59),
+            self.reset_traded_today,
+        )
+
+    def reset_traded_today(self):
+        self.traded_today = False
 
     def pip_value(self, current_price):
         return (self.pip_size / current_price) * self.lot_size
@@ -38,34 +46,35 @@ class OcODevisenStrategy(QCAlgorithm):
     def is_place_order_time(self) -> bool:
         return time(9, 0) > self.time.time() > time(8, 00)
 
-    # TODO check if this works the same way for forex
-    # need to convert the pip distance into lot
-    # if you want to risk 100 euro
-    # and one lot is 100.000 euro -> 0.001 lot
+    # TODO guard against running out of margin
     def position_size(self, stop_loss_distance, bar):
         risk_per_trade = self.portfolio.total_portfolio_value * 0.01
         risk_of_pip_distance = stop_loss_distance * bar.close
         lots = risk_per_trade / risk_of_pip_distance
-        print("stop here")
         return lots
 
-    def stop_loss_distance(self, bar):
-        return bar.close - self.stop_loss_price_long()
+    def stop_loss_distance(self, bar, direction: str):
+        if direction == "long":
+            return bar.close - self.stop_loss_price_long()
+        if direction == "short":
+            return self.stop_loss_price_short() - bar.close
 
     def stop_loss_price_long(self):
         return self.last_60_min_low() - self._atr.current.value
 
-    def stop_loss_price_short(self, bar):
+    def stop_loss_price_short(self):
         return self.last_60_min_high() + self._atr.current.value
 
     def take_profit_price_long(self, bar):
         return bar.close + (
-            self.stop_loss_distance(bar) * float(self.get_parameter("reward_factor"))
+            self.stop_loss_distance(bar, "long")
+            * float(self.get_parameter("reward_factor"))
         )
 
     def take_profit_price_short(self, bar):
         return bar.close - (
-            self.stop_loss_distance(bar) * float(self.get_parameter("reward_factor"))
+            self.stop_loss_distance(bar, "short")
+            * float(self.get_parameter("reward_factor"))
         )
 
     def last_60_min_low(self):
@@ -87,29 +96,29 @@ class OcODevisenStrategy(QCAlgorithm):
             )
 
     def on_data(self, data: Slice):
-        if not data.contains_key(self._eur_usd_symbol):
+        if self.traded_today or not data.contains_key(self._eur_usd_symbol):
             return
 
         bar = data[self._eur_usd_symbol]
 
         self.bar_window.add(bar)
+
         if not self.portfolio.invested and self.is_place_order_time():
             if self.break_out(direction="long"):
                 self.market_order(
                     symbol=self._eur_usd_symbol,
                     quantity=self.position_size(
-                        stop_loss_distance=self.stop_loss_distance(bar=bar), bar=bar
+                        stop_loss_distance=self.stop_loss_distance(
+                            bar=bar, direction="long"
+                        ),
+                        bar=bar,
                     ),
                 )
-                # enter with market order with 1% portfolio
-                # TODO self.traded_today = True
-                # register take profit
                 take_profit_ticket = self.LimitOrder(
                     self._eur_usd_symbol,
                     -self.Portfolio[self._eur_usd_symbol].Quantity,
                     self.take_profit_price_long(bar=bar),
                 )
-                # register stop loss
                 stop_loss_ticket = self.StopMarketOrder(
                     self._eur_usd_symbol,
                     -self.Portfolio[self._eur_usd_symbol].Quantity,
@@ -122,17 +131,14 @@ class OcODevisenStrategy(QCAlgorithm):
                 self.market_order(
                     symbol=self._eur_usd_symbol,
                     quantity=-self.position_size(
-                        self.stop_loss_distance(bar=bar), bar=bar
+                        self.stop_loss_distance(bar=bar, direction="short"), bar=bar
                     ),
-                )  # enter with market order with 1% portfolio
-                # TODO self.traded_today = True
-                # register take profit
+                )
                 take_profit_ticket = self.LimitOrder(
                     self._eur_usd_symbol,
                     self.Portfolio[self._eur_usd_symbol].Quantity,
                     self.take_profit_price_short(bar=bar),
                 )
-                # register stop loss
                 stop_loss_ticket = self.StopMarketOrder(
                     self._eur_usd_symbol,
                     self.Portfolio[self._eur_usd_symbol].Quantity,
@@ -141,7 +147,6 @@ class OcODevisenStrategy(QCAlgorithm):
                 self.register_oco_orders(take_profit_ticket, stop_loss_ticket)
 
     def register_oco_orders(self, take_profit_ticket, stop_loss_ticket):
-        # set up order index so we can cancel the opposite once filled
         self.orders[stop_loss_ticket.order_id] = {
             "oco_order_id": take_profit_ticket.order_id,
             "type": "stop_loss",
@@ -158,5 +163,11 @@ class OcODevisenStrategy(QCAlgorithm):
                 self.transactions.cancel_order(order["oco_order_id"])
                 if order["type"] == "take_profit":
                     self.winning_trades += 1
-            else:  # plot entry
+            else:
                 self.total_trades += 1
+                self.traded_today = True
+
+    def on_end_of_algorithm(self):
+        if self.total_trades > 0:
+            hit_rate = self.winning_trades / self.total_trades
+            self.debug(f"Hit Rate: {hit_rate:.2%}")
