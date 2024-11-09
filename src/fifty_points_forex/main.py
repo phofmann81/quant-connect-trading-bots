9fb49ba054a8180c2dace4044a119d66d316b181
Philipp Hofmann <philipp.hofmann@zalando.de>
2024-11-09 21:27:57 +0100

    wip2

--- src/fifty_points_forex/main.py
+++ src/fifty_points_forex/main.py
@@ -1,7 +1,7 @@
 # region imports
 from AlgorithmImports import *
-from breakout_alpha_model import BreakoutAlphaModel
 from symbol_data import SymbolData
+from trailing_stop_risk import TrailingStopRiskManagementModel
 
 # endregion
 
@@ -13,6 +13,7 @@ class OcODevisenStrategy(QCAlgorithm):
         self.set_end_date(2024, 10, 17)
 
         self.set_cash(100000)
+        self.default_order_properties.time_in_force = TimeInForce.DAY
 
         berlin_time_zone_utc_plus_2 = "Europe/Berlin"
         self.set_time_zone(berlin_time_zone_utc_plus_2)
@@ -23,7 +24,7 @@ class OcODevisenStrategy(QCAlgorithm):
         )
 
         symbols: List[Symbol] = [
-            self.add_forex(ticker=currency_pair).symbol
+            self.add_forex(ticker=currency_pair, resolution=Resolution.HOUR).symbol
             for currency_pair in ["EURUSD", "GBPUSD", "EURGBP"]
         ]
 
@@ -33,90 +34,11 @@ class OcODevisenStrategy(QCAlgorithm):
         self.symbol_data: Mapping[Symbol, SymbolData] = {}
 
         self.pip = 0.0001  # TODO make this dependend on currency pair, this is not correct for Yen
-
-        self.set_risk_management(TrailingStopRiskManagementModel())
+        self.lot_size = 100000
+        self.set_risk_management(TrailingStopRiskManagementModel(0.05))
+        self.set_execution(ImmediateExecutionModel())
 
         self.orders = {}
-        # self.bar_window = RollingWindow[QuoteBar](
-        #     61
-        # )  # TODO learn how to use consolidator for this
-
-        # self.pip_size = (
-        #     self._eur_usd.symbol_properties.minimum_price_variation * 10
-        # )
-        # self.pip_size_breakout_padding = self.pip_size * 2
-        # self.lot_size = (
-        #     int(1 / self._eur_usd.symbol_properties.minimum_price_variation) + 1
-
-    #     # )
-    #     self.total_trades = 0
-    #     self.winning_trades = 0
-    #     self.traded_today = False
-
-    #     #     self.schedule.on(
-    #     #         self.date_rules.every_day(), self.time_rules.at(22, 59), self.liquidate
-    #     #     )
-    #     self.schedule.on(
-    #         self.date_rules.every_day(),
-    #         self.time_rules.at(23, 59),
-    #         self.reset_traded_today,
-    #     )
-
-    # def reset_traded_today(self):
-    #     self.traded_today = False
-
-    # def pip_value(self, current_price):
-    #     return (self.pip_size / current_price) * self.lot_size
-
-    # def is_place_order_time(self) -> bool:
-    #     return time(9, 0) > self.time.time() > time(8, 00)
-
-    # # TODO guard against running out of margin
-    # def position_size(self, stop_loss_distance, bar):
-    #     risk_per_trade = self.portfolio.total_portfolio_value * 0.01
-    #     risk_of_pip_distance = stop_loss_distance * bar.close
-    #     lots = risk_per_trade / risk_of_pip_distance
-    #     return lots
-
-    # def stop_loss_distance(self, bar, direction: str):
-    #     if direction == "long":
-    #         return bar.close - self.stop_loss_price_long()
-    #     if direction == "short":
-    #         return self.stop_loss_price_short() - bar.close
-
-    # def stop_loss_price_long(self):
-    #     return self.last_60_min_low() - self._atr.current.value
-
-    # def stop_loss_price_short(self):
-    #     return self.last_60_min_high() + self._atr.current.value
-
-    # def take_profit_price_long(self, bar):
-    #     return bar.close + (
-    #         self.stop_loss_distance(bar, "long") * float(self.get_parameter("reward_factor"))
-    #     )
-
-    # def take_profit_price_short(self, bar):
-    #     return bar.close - (
-    #         self.stop_loss_distance(bar, "short") * float(self.get_parameter("reward_factor"))
-    #     )
-
-    # def last_60_min_low(self):
-    #     return min(bar.low for bar in list(self.bar_window)[1:])
-
-    # def last_60_min_high(self):
-    #     return max(bar.high for bar in list(self.bar_window)[1:])
-
-    # def break_out(self, direction: str) -> bool:
-    #     if direction == "long":
-    #         return (
-    #             self.bar_window[0].close
-    #             > self.last_60_min_high() + self.pip_size_breakout_padding
-    #         )
-    #     if direction == "short":
-    #         return (
-    #             self.bar_window[0].close
-    #             < self.last_60_min_low() - self.pip_size_breakout_padding
-    #         )
 
     def get_quantity(self, quote_bar: QuoteBar) -> Mapping[str, float]:
         def get_maximum_loss(price1: float, price2: float) -> float:
@@ -135,10 +57,11 @@ class OcODevisenStrategy(QCAlgorithm):
             return available_margin / margin_ratio
 
         def calculate_position_size(
-            max_margin: float, risk_exposure: float, max_loss: float, close_price: float
+            max_margin: float, risk_exposure: float, max_loss: float
         ) -> float:
-            position_size = risk_exposure / (max_loss * close_price)
-            return min(position_size, max_margin)
+            position_size = risk_exposure / max_loss
+            desired_size = min(position_size, max_margin)
+            return max(round(desired_size / self.lot_size), 1) * self.lot_size
 
         risk_exposure = self.portfolio.total_portfolio_value * 0.01
 
@@ -150,11 +73,9 @@ class OcODevisenStrategy(QCAlgorithm):
         max_margin = calculate_max_margin()
 
         # Calculate buy and sell sizes, constrained by max margin
-        buy_size = calculate_position_size(
-            max_margin, risk_exposure, maximum_loss_buy, quote_bar.close
-        )
+        buy_size = calculate_position_size(max_margin, risk_exposure, maximum_loss_buy)
         sell_size = calculate_position_size(
-            max_margin, risk_exposure, maximum_loss_sell, quote_bar.close
+            max_margin, risk_exposure, maximum_loss_sell
         )
 
         return {"buy": buy_size, "sell": sell_size}
@@ -165,65 +86,34 @@ class OcODevisenStrategy(QCAlgorithm):
             for symbol, symbol_data in self.symbol_data.items():
                 bar = data[symbol]
 
+                buy_stop_price = bar.high + self.pip * 2
                 # entry tickets
                 buy_stop_ticket = self.stop_market_order(
-                    symbol, self.get_quantity(bar)["buy"], bar.close + self.pip * 2
+                    symbol=symbol,
+                    quantity=self.get_quantity(bar)["buy"],
+                    stop_price=buy_stop_price,
                 )
+
+                sell_stop_price = bar.low - self.pip * 2
                 sell_stop_ticket = self.stop_market_order(
-                    symbol, self.get_quantity(bar)["sell"], bar.close - self.pip * 2
+                    symbol, -self.get_quantity(bar)["sell"], sell_stop_price
                 )
-                self.register_oco_orders(buy_stop_ticket, sell_stop_ticket)
+                self.register_oco_orders(
+                    buy_stop_ticket, sell_stop_ticket
+                )  # TODO test if not cancelling is more profitable
 
                 # stop loss tickets
                 buy_stop_loss_ticket = self.stop_market_order(
-                    symbol, -self.portfolio[symbol].quantity, bar.low
+                    symbol,
+                    -self.portfolio[symbol].quantity,
+                    min(bar.low, buy_stop_price - 6 * self.pip),
                 )
                 sell_stop_loss_ticket = self.stop_market_order(
-                    symbol, -self.portfolio[symbol].quantity, bar.high
+                    symbol,
+                    -self.portfolio[symbol].quantity,
+                    max(bar.high, sell_stop_price + 6 * self.pip),
                 )
                 self.register_oco_orders(buy_stop_loss_ticket, sell_stop_loss_ticket)
-
-        # self.bar_window.add(bar)
-
-        # if not self.portfolio.invested and self.is_place_order_time():
-        #     if self.break_out(direction="long"):
-        #         self.market_order(
-        #             symbol=self._eur_usd_symbol,
-        #             quantity=self.position_size(
-        #                 stop_loss_distance=self.stop_loss_distance(bar=bar, direction="long"), bar=bar
-        #             ),
-        #         )
-        #         take_profit_ticket = self.LimitOrder(
-        #             self._eur_usd_symbol,
-        #             -self.Portfolio[self._eur_usd_symbol].Quantity,
-        #             self.take_profit_price_long(bar=bar),
-        #         )
-        #         stop_loss_ticket = self.StopMarketOrder(
-        #             self._eur_usd_symbol,
-        #             -self.Portfolio[self._eur_usd_symbol].Quantity,
-        #             self.stop_loss_price_long(),
-        #         )
-        #         self.register_oco_orders(take_profit_ticket, stop_loss_ticket)
-
-        #     elif self.break_out(direction="short"):
-        #         # TODO refactor into bracket order to remove duplication
-        #         self.market_order(
-        #             symbol=self._eur_usd_symbol,
-        #             quantity=-self.position_size(
-        #                 self.stop_loss_distance(bar=bar, direction="short"), bar=bar
-        #             ),
-        #         )
-        #         take_profit_ticket = self.LimitOrder(
-        #             self._eur_usd_symbol,
-        #             -self.Portfolio[self._eur_usd_symbol].Quantity,
-        #             self.take_profit_price_short(bar=bar),
-        #         )
-        #         stop_loss_ticket = self.StopMarketOrder(
-        #             self._eur_usd_symbol,
-        #             -self.Portfolio[self._eur_usd_symbol].Quantity,
-        #             self.stop_loss_price_short(),
-        #         )
-        #         self.register_oco_orders(take_profit_ticket, stop_loss_ticket)
 
     def register_oco_orders(self, one_ticket, other_ticket):
         self.orders[one_ticket.order_id] = {
@@ -240,11 +130,6 @@ class OcODevisenStrategy(QCAlgorithm):
         if order_event.status == OrderStatus.FILLED:
             if (order := self.orders.get(order_event.order_id)) is not None:  # exit
                 self.transactions.cancel_order(order["oco_order_id"])
-
-    # def on_end_of_algorithm(self):
-    #     if self.total_trades > 0:
-    #         hit_rate = self.winning_trades / self.total_trades
-    #         self.debug(f"Hit Rate: {hit_rate:.2%}")
 
     def on_securities_changed(self, changes):
         for security in changes.AddedSecurities:
